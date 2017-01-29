a24b2c2ada5157830b854592042f132e6bcbdfef
klnvch <klnvch@gmail.com>
2017-01-29 22:47:21 +0100

    restore readme

--- main_4.py
+++ main_4.py
@@ -10,41 +10,42 @@ import numpy as np
 
 DIR_TEST = 'tests/'
 
-FILE_TEST_OLD = ['sigmoid_gradient_0.1_20000_256_[24]_1481742134',
-             'sigmoid_gradient_0.1_20000_256_[28]_1481744074',
-             'sigmoid_adagrad_0.01_20000_256_[28]_rs_1482193004',
-             'sigmoid_adagrad_0.01_20000_512_[28]_rs_1482200579',
-             'sigmoid_gradient_0.1_20000_256_[32]_1481746487',
-             'sigmoid_adagrad_0.01_20000_256_[32]_rs_1482194907',
-             'sigmoid_adagrad_0.01_20000_512_[32]_rs_1482202214',
-             'sigmoid_gradient_0.1_20000_256_[36]_1481748759',
-             'sigmoid_adagrad_0.01_20000_256_[36]_rs_1482196776',
-             'sigmoid_adagrad_0.01_20000_512_[36]_rs_1482203863',
-             'sigmoid_adagrad_0.01_20000_256_[40]_rs_1482198668',
-             'sigmoid_adagrad_0.01_20000_512_[40]_rs_1482205541',
-             'softmax_gradient_0.01_10000_256_[24]_1481753606',
-             'softmax_gradient_0.01_10000_256_[28]_1481754757',
-             'softmax_adagrad_0.01_20000_256_[28]_rs_1482207235',
-             'softmax_gradient_0.01_10000_256_[32]_1481755922',
-             'softmax_adagrad_0.01_20000_256_[32]_rs_1482209491',
-             'softmax_gradient_0.01_10000_256_[36]_1481757079',
-             'softmax_adagrad_0.01_20000_256_[36]_rs_1482211759',
-             'softmax_adagrad_0.01_20000_256_[40]_rs_1482214074',
-             'softmax_gradient_0.01_10000_256_[10, 10]_1481834319',
-             'sigmoid_gradient_0.1_10000_256_[16, 16]_1481835700',
-             'sigmoid_adam_0.1_20000_256_[16, 16]_1481837697',
-             'sigmoid_adagrad_0.1_10000_256_[16, 16]_1481842808',
-             'sigmoid_adagrad_0.1_10000_256_[16, 16]_s_1481843900',
-             'sigmoid_gradient_0.01_20000_256_[20, 20]_rs_1482216396',
-             'sigmoid_adam_0.01_20000_256_[20, 20]_rs_1482218326',
-             'sigmoid_adagrad_0.01_20000_256_[20, 20]_rs_1482220406',
-             'sigmoid_adam_0.01_2000_128_[20, 20]_rs_gpu_1482258641',
-             'sigmoid_adam_0.01_2000_512_[20, 20]_rs_gpu_1482259458',
-             'sigmoid_adam_0.01_2000_128_[20, 20]_rs_1482259957',
-             'sigmoid_adam_0.01_2000_512_[20, 20]_rs_1482260557']
-
-FILE_TEST = ['sigmoid_adam_0.01_2000_128_[20, 20]_1482340044',
-             'sigmoid_adam_0.01_2000_128_[20, 20]_1482340285']
+FILE_TEST = [
+    'sigmoid_gradient_0.1_20000_256_[24]_1481742134',
+    'sigmoid_gradient_0.1_20000_256_[28]_1481744074',
+    'sigmoid_adagrad_0.01_20000_256_[28]_rs_1482193004',
+    'sigmoid_adagrad_0.01_20000_512_[28]_rs_1482200579',
+    'sigmoid_gradient_0.1_20000_256_[32]_1481746487',
+    'sigmoid_adagrad_0.01_20000_256_[32]_rs_1482194907',
+    'sigmoid_adagrad_0.01_20000_512_[32]_rs_1482202214',
+    'sigmoid_gradient_0.1_20000_256_[36]_1481748759',
+    'sigmoid_adagrad_0.01_20000_256_[36]_rs_1482196776',
+    'sigmoid_adagrad_0.01_20000_512_[36]_rs_1482203863',
+    'sigmoid_adagrad_0.01_20000_256_[40]_rs_1482198668',
+    'sigmoid_adagrad_0.01_20000_512_[40]_rs_1482205541',
+    'softmax_gradient_0.01_10000_256_[24]_1481753606',
+    'softmax_gradient_0.01_10000_256_[28]_1481754757',
+    'softmax_adagrad_0.01_20000_256_[28]_rs_1482207235',
+    'softmax_gradient_0.01_10000_256_[32]_1481755922',
+    'softmax_adagrad_0.01_20000_256_[32]_rs_1482209491',
+    'softmax_gradient_0.01_10000_256_[36]_1481757079',
+    'softmax_adagrad_0.01_20000_256_[36]_rs_1482211759',
+    'softmax_adagrad_0.01_20000_256_[40]_rs_1482214074',
+    'softmax_gradient_0.01_10000_256_[10, 10]_1481834319',
+    'sigmoid_gradient_0.1_10000_256_[16, 16]_1481835700',
+    'sigmoid_adam_0.1_20000_256_[16, 16]_1481837697',
+    'sigmoid_adagrad_0.1_10000_256_[16, 16]_1481842808',
+    'sigmoid_adagrad_0.1_10000_256_[16, 16]_s_1481843900',
+    'sigmoid_gradient_0.01_20000_256_[20, 20]_rs_1482216396',
+    'sigmoid_adam_0.01_20000_256_[20, 20]_rs_1482218326',
+    'sigmoid_adagrad_0.01_20000_256_[20, 20]_rs_1482220406',
+    'sigmoid_adam_0.01_2000_128_[20, 20]_rs_gpu_1482258641',
+    'sigmoid_adam_0.01_2000_512_[20, 20]_rs_gpu_1482259458',
+    'sigmoid_adam_0.01_2000_128_[20, 20]_rs_1482259957',
+    'sigmoid_adam_0.01_2000_512_[20, 20]_rs_1482260557',
+    'sigmoid_adam_0.01_2000_128_[20, 20]_1482340044',
+    'sigmoid_adam_0.01_2000_128_[20, 20]_1482340285'
+    ]
 
 def draw_curve(x, y, lgnd, use_max=True):
     if use_max: i = y.argmax()
@@ -69,11 +70,22 @@ def print_results(test, data):
 
 
 if __name__ == '__main__':
+    
+    file = open('README.md', 'w')
+    print('#Rejection Option', file=file)
+    print('', file=file)
+    print('| Test | Steps | Loss | Trn acc | Vld acc | Area 0 | Area 1 | Area 2 | Time |', file=file)
+    print('| ---- | ----- | ---- | ------- | ------- | ------ | ------ | ------ | ---- |', file=file)
+    
     for test in FILE_TEST:
         with open(DIR_TEST + test + '.csv', 'r') as f:
             data = list(reader(f))
             data = np.array(data)
             print_results(test, data)
+            print('| {:s} | {:6d} | {:8f} | {:8f} | {:8f} | {:8f} | {:8f} | {:8f} | {:4d} |'
+              .format(test, data[-1,0].astype(np.int), data[-1,1].astype(np.float), data[-1,2].astype(np.float),
+                      data[-1,3].astype(np.float), data[-1,4].astype(np.float), data[-1,5].astype(np.float),
+                      data[-1,6].astype(np.float), int(np.sum(data[1:,7].astype(np.float)) / 60.0)), file=file)
         
             x = data[1:,0]
             
@@ -94,5 +106,10 @@ if __name__ == '__main__':
             #plt.show()
             plt.gcf().set_size_inches(11.69, 8.27)
             plt.savefig('/home/anton/workspace2/DiplomaWork/images/' + test + '.png')
+            
+    print('', file=file)
+    for test in FILE_TEST:
+        print('![](images/' + test + '.png)', file=file)
+    file.close()
         
         
