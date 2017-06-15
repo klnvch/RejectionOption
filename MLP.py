3fde7476d6318612523de4d8e5fc138d96776b73
klnvch <klnvch@gmail.com>
2017-06-15 12:56:06 +0300

    add dropout

--- MLP.py
+++ MLP.py
@@ -4,12 +4,6 @@ Created on Oct 26, 2016
 @author: anton
 
 This module keeps all code for Tensorflow to implement MLP
-
-TODO:
-    1. create a copy using tf.contrib.learn
-    2. check what is skflow
-    3. simplify
-    4. more comments
 '''
 
 import time, os
@@ -18,47 +12,75 @@ from sklearn.utils import shuffle
 
 class MLP:
     
-    def __init__(self, learning_rate, layers, activation_function='softmax',
+    def __init__(self, learning_rate, layers, last_layer='softmax',
                  optimizer_name=None, beta=0.0):
-        print('init...')
-        print('learning rate: {:g}, activation function: {:s}, optimizer: {:s}'.format(learning_rate, activation_function, optimizer_name))
+        print('create neural network...')
+        log_msg = 'learning rate: {:g}, function: {:s}, optimizer: {:s}'
+        log_msg = log_msg.format(learning_rate, last_layer, optimizer_name)
+        print(log_msg)
         print('layers: {:s}'.format(str(layers)))
         
         self.learning_rate = learning_rate
         self.optimizer_name = optimizer_name
-        self.activation_function = activation_function
+        self.activation_function = last_layer
         self.num_input = layers[0]
         self.num_hidden = layers[1:-1]
         self.num_output = layers[-1]
         
         self.x = tf.placeholder(tf.float32, [None, self.num_input])
         self.y_ = tf.placeholder(tf.float32, [None, self.num_output])
+        self.keep_prob = tf.placeholder(tf.float32)
         
         if len(self.num_hidden) == 0:
-            y, regularizers = self.add_layer(self.x, [self.num_input, self.num_output], None, '')
+            y, r1 = self.add_layer(self.x,
+                                   [self.num_input, self.num_output],
+                                   None, '', self.keep_prob)
+            regularizers = r1
             
         elif len(self.num_hidden) == 1:
-            h, r1 = self.add_layer(self.x, [self.num_input,     self.num_hidden[0]], tf.nn.sigmoid, '1')
-            y, r2 = self.add_layer(h,      [self.num_hidden[0], self.num_output],    None,          '2')
-            regularizers = r1 +r2
+            h, r1 = self.add_layer(self.x,
+                                   [self.num_input, self.num_hidden[0]],
+                                   tf.nn.sigmoid, '1', self.keep_prob)
+            y, r2 = self.add_layer(h,
+                                   [self.num_hidden[0], self.num_output],
+                                   None, '2', self.keep_prob)
+            regularizers = r1 + r2
             
         elif len(self.num_hidden) == 2:
-            h1, r1 = self.add_layer(self.x, [self.num_input,     self.num_hidden[0]], tf.nn.relu, '1')
-            h2, r2 = self.add_layer(h1,     [self.num_hidden[0], self.num_hidden[1]], tf.nn.relu, '2')
-            y, r3  = self.add_layer(h2,     [self.num_hidden[1], self.num_output],    None,       '3')
-            regularizers = r1 +r2 + r3
+            h1, r1 = self.add_layer(self.x,
+                                    [self.num_input, self.num_hidden[0]],
+                                    tf.nn.relu, '1', self.keep_prob)
+            h2, r2 = self.add_layer(h1,
+                                    [self.num_hidden[0], self.num_hidden[1]],
+                                    tf.nn.relu, '2', self.keep_prob)
+            y, r3 = self.add_layer(h2,
+                                    [self.num_hidden[1], self.num_output],
+                                    None, '3', self.keep_prob)
+            regularizers = r1 + r2 + r3
             
         elif len(self.num_hidden) == 3:
-            h1, r1 = self.add_layer(self.x, [self.num_input,     self.num_hidden[0]], tf.nn.relu, '1')
-            h2, r2 = self.add_layer(h1,     [self.num_hidden[0], self.num_hidden[1]], tf.nn.relu, '2')
-            h3, r3 = self.add_layer(h2,     [self.num_hidden[1], self.num_hidden[2]], tf.nn.relu, '3')
-            y, r4  = self.add_layer(h3,     [self.num_hidden[2], self.num_output],    None,       '4')
+            h1, r1 = self.add_layer(self.x,
+                                    [self.num_input, self.num_hidden[0]],
+                                    tf.nn.relu, '1', self.keep_prob)
+            h2, r2 = self.add_layer(h1,
+                                    [self.num_hidden[0], self.num_hidden[1]],
+                                    tf.nn.relu, '2', self.keep_prob)
+            h3, r3 = self.add_layer(h2,
+                                    [self.num_hidden[1], self.num_hidden[2]],
+                                    tf.nn.relu, '3', self.keep_prob)
+            y, r4 = self.add_layer(h3,
+                                    [self.num_hidden[2], self.num_output],
+                                    None, '4', self.keep_prob)
             regularizers = r1 + r2 + r3 + r4
             
-        if activation_function == 'softmax':
-            self.loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=self.y_, logits=y))
-        elif activation_function == 'sigmoid':
-            self.loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=self.y_, logits=y))
+        if last_layer == 'softmax':
+            self.loss = tf.reduce_mean(
+                tf.nn.softmax_cross_entropy_with_logits(labels=self.y_,
+                                                        logits=y))
+        elif last_layer == 'sigmoid':
+            self.loss = tf.reduce_mean(
+                tf.nn.sigmoid_cross_entropy_with_logits(labels=self.y_,
+                                                        logits=y))
         
         self.loss = tf.reduce_mean(self.loss + beta * regularizers)
         
@@ -69,7 +91,8 @@ class MLP:
         elif optimizer_name == 'Adagrad':
             optimizer = tf.train.AdagradOptimizer(learning_rate)
         elif optimizer_name == 'Momentum':
-            optimizer = tf.train.MomentumOptimizer(learning_rate, 0.9, use_nesterov=True) 
+            optimizer = tf.train.MomentumOptimizer(learning_rate, 0.9,
+                                                   use_nesterov=True) 
         elif optimizer_name == 'Adam':
             optimizer = tf.train.AdamOptimizer(learning_rate)
         elif optimizer_name == 'Ftrl':
@@ -79,35 +102,37 @@ class MLP:
         
         self.train_step = optimizer.minimize(self.loss)
         
-        #grads_and_vars = optimizer.compute_gradients(self.loss)
-        #grad_norms = [tf.nn.l2_loss(g) for g, _ in grads_and_vars]
-        #self.grad_norm = tf.add_n(grad_norms)
-    
+        # grads_and_vars = optimizer.compute_gradients(self.loss)
+        # grad_norms = [tf.nn.l2_loss(g) for g, _ in grads_and_vars]
+        # self.grad_norm = tf.add_n(grad_norms)
+        
         self.y_final = None
-        if   activation_function == 'softmax': self.y_final = tf.nn.softmax(y)
-        elif activation_function == 'sigmoid': self.y_final = tf.nn.sigmoid(y)
-        else: raise ValueError('no such activation function: ' + activation_function)
+        if   last_layer == 'softmax': self.y_final = tf.nn.softmax(y)
+        elif last_layer == 'sigmoid': self.y_final = tf.nn.sigmoid(y)
+        else: raise ValueError('wrong function  value: ' + last_layer)
         
-        self.correct_prediction = tf.equal(tf.argmax(self.y_final, 1), tf.argmax(self.y_, 1))
-        self.accuracy = tf.reduce_mean(tf.cast(self.correct_prediction, tf.float32))
-        
-        
-    def add_layer(self, x, shape, activation_function, postfix_name):
-        w = tf.Variable(tf.truncated_normal(shape, stddev=0.1), 
+        y_true = tf.equal(tf.argmax(self.y_final, 1), tf.argmax(self.y_, 1))
+        self.accuracy = tf.reduce_mean(tf.cast(y_true, tf.float32))
+    
+    def add_layer(self, x, shape, activation_function, postfix_name, keep_prob):
+        w = tf.Variable(tf.truncated_normal(shape, stddev=0.1),
                         name='weights_' + postfix_name)
-        b = tf.Variable(tf.constant(0.1, shape=[shape[1]]), 
+        b = tf.Variable(tf.constant(0.1, shape=[shape[1]]),
                         name='biases_' + postfix_name)
         l2_loss = tf.nn.l2_loss(w)
         if activation_function is None:
             return tf.matmul(x, w) + b, l2_loss
         else:
-            return activation_function(tf.matmul(x, w) + b), l2_loss
-        
-        
-    def train(self, steps, trn, vld=None, batch_size=None, log=True):
+            layer = activation_function(tf.matmul(x, w) + b)
+            layer = tf.nn.dropout(layer, keep_prob)
+            return layer, l2_loss
+    
+    def train(self, steps, trn, vld=None, batch_size=None, keep_prob=1.0,
+              log=True):
         """
         if early stopping not None output is
-            [model_file, step, loss, trn_acc, vld_acc, area] for best_vld_acc, best_area0, best_area1 and best_area2
+            [model_file, step, loss, trn_acc, vld_acc, area] for best_vld_acc,
+             best_area0, best_area1 and best_area2
         """
         self.clean_model_dir()
         self.log_init(steps, batch_size, log)
@@ -120,20 +145,23 @@ class MLP:
         step = 0
         with tf.Session() as sess:
             tf.global_variables_initializer().run()
-            for step in range(steps+1):
+            for step in range(steps + 1):
                 # train 
                 start_time = time.time()
                 x, y = shuffle(trn.x, trn.y)
                 if batch_size is None:
-                    sess.run(self.train_step, feed_dict={self.x: x, self.y_: y})
+                    sess.run(self.train_step,
+                             feed_dict={self.x: x, self.y_: y,
+                                        self.keep_prob: keep_prob})
                 else:
                     for i in range(0, trn.x.shape[0], batch_size):
-                        sess.run(self.train_step, 
-                                 feed_dict={self.x: x[i:i+batch_size],
-                                            self.y_: y[i:i+batch_size]})
+                        sess.run(self.train_step,
+                                 feed_dict={self.x: x[i:i + batch_size],
+                                            self.y_: y[i:i + batch_size],
+                                            self.keep_prob: keep_prob})
                 finish_time = time.time()
                 dt += (finish_time - start_time)
-                if step%100 == 0:
+                if step % 100 == 0:
                     loss, trn_acc, vld_acc = \
                         self.log_step_info(sess, trn, vld, step, dt, log)
                     dt = 0
@@ -162,77 +190,66 @@ class MLP:
         with tf.Session() as sess:
             saver.restore(sess, filename)
             return sess.run(self.accuracy,
-                            feed_dict={self.x: tst.x, self.y_: tst.y})
+                            feed_dict={self.x: tst.x,
+                                       self.y_: tst.y,
+                                       self.keep_prob: 1.0})
     
     def predict_proba(self, x, filename='saver/model.ckpt'):
         if x is None: return None
         saver = tf.train.Saver()
         with tf.Session() as sess:
             saver.restore(sess, filename)
-            return sess.run(self.y_final, feed_dict={self.x: x})
-        
+            return sess.run(self.y_final, feed_dict={self.x: x,
+                                                     self.keep_prob: 1.0})
+    
     def predict(self, x, filename='saver/model.ckpt'):
         saver = tf.train.Saver()
         with tf.Session() as sess:
             saver.restore(sess, filename)
             return sess.run(self.y_final, feed_dict={self.x: x})
-        
-            
+    
     def clean_model_dir(self):
-        dirPath = "saver/"
+        dirPath = 'saver/'
         fileList = os.listdir(dirPath)
         for fileName in fileList:
-            os.remove(dirPath+"/"+fileName)
+            os.remove(dirPath + '/' + fileName)
     
     def log_init(self, steps, batch_size, logging):
-        
         self.log_file = None
         if logging:
-            filename = 'tests/{:s}_{:s}_{:g}_{:d}_{:d}_{:s}_{:d}.csv'.format(self.activation_function, self.optimizer_name, self.learning_rate, steps, batch_size, str(self.num_hidden), int(time.time()))
+            filename = 'tests/{:s}_{:s}_{:g}_{:d}_{:d}_{:s}_{:d}.csv'
+            filename.format(self.activation_function, self.optimizer_name,
+                            self.learning_rate, steps, batch_size,
+                            str(self.num_hidden), int(time.time()))
             self.log_file = open(filename, 'w+')
-            print('Step,Loss,Train accuracy,Validation accuracy,ROC AUC: output threshold,ROC AUC: differential threshold,ROC AUC: ratio threshold,Time', file=self.log_file)
-    
-        log_msg = 'learing rate: {:f}; steps: {:d}'.format(self.learning_rate, steps)
+            print('Step,Loss,Train accuracy,Validation accuracy,'\
+                  'ROC AUC: output threshold,ROC AUC: differential threshold,'\
+                  'ROC AUC: ratio threshold,Time', file=self.log_file)
+        
+        log_msg = 'learing rate: {:f}; steps: {:d}'
+        log_msg = log_msg.format(self.learning_rate, steps)
         if batch_size is not None:
             log_msg += '; batch size: {:d}'.format(batch_size)
         print(log_msg)
-        
+    
     def log_step_info(self, sess, trn, vld, step, train_time, logging):
-        loss, trn_acc = sess.run([self.loss, self.accuracy], feed_dict={self.x: trn.x, self.y_: trn.y})
+        loss, trn_acc = sess.run([self.loss, self.accuracy],
+                                 feed_dict={self.x: trn.x,
+                                            self.y_: trn.y,
+                                            self.keep_prob: 1.0})
         if vld is not None:
-            vld_acc = sess.run(self.accuracy, feed_dict={self.x: vld.x, self.y_: vld.y})
+            vld_acc = sess.run(self.accuracy,
+                               feed_dict={self.x: vld.x,
+                                          self.y_: vld.y,
+                                          self.keep_prob: 1.0})
         else: vld_acc = 0
         
-        log_msg = '{:8d}, {:9f}, {:9f}, {:9f}, {:f}'.format(step, loss, trn_acc, vld_acc, train_time)
+        log_msg = '{:8d}, {:9f}, {:9f}, {:9f}, {:f}'
+        log_msg = log_msg.format(step, loss, trn_acc, vld_acc, train_time)
         if logging: print(log_msg, file=self.log_file)
         print(log_msg)
         
         return loss, trn_acc, vld_acc
-    
-            
-    def log_accuracy(self, i, loss, trn_acc, vld_acc=None, areas=None, mark=None, logging=True):
-        
-        if vld_acc != 0:
-            log_msg = 'step{:8d}, loss:{:9f}, trn_acc:{:9f}'.format(i, loss, trn_acc)
-            if vld_acc is not None:
-                log_msg += ', vld_acc{:9f}'.format(vld_acc)
-            if areas is not None:
-                log_msg += ', r0:{:12f}, r1:{:12f}, r2:{:12f}'.format(areas[1], areas[2], areas[3])
-            if mark is not None:
-                log_msg += ', {:s}'.format(mark)
-            if logging: print(log_msg, file=self.log_file)
-            print(log_msg)
-        else:
-            log_msg = "step %d; loss %g; train accuracy: %g"%(i, loss, trn_acc)
-            if logging: print(log_msg, file=self.log_file)
-            else:       print(log_msg)
-            
-    def log_results(self, threshold, c, e , r_c, r_e, logging):
-        
-        log_msg = "threshold %g; correct: %d; errors: %d; reject correct: %d; reject error: %d"%(threshold, len(c), len(e), len(r_c), len(r_e))
-        if logging: print(log_msg, file=self.log_file)
-        else:       print(log_msg)
-        
     
     def log_finish(self):
         print('train finished')
